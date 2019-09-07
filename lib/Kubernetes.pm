8e08ba422937cbd05a9fa54e17a29d4bc9f560b5
Sterling Hanenkamp <sterling@hanenkamp.com>
2019-09-07 15:20:09 -0500

    Adding bits to make Kubernetes installable

---
+++ lib/Kubernetes.pm
@@ -0,0 +1,15 @@
+require 5.6.0;
+use strict;
+use warnings;
+
+our $VERSION = '1.13.7';
+
+=head1 NAME
+
+Kubernetes - auto-generated Kubernetes interface library
+
+=head1 DESCRIPTION
+
+This is forked from L<https://github.com/kubernetes-client/perl> and has the required bits added to allow cpanm to install it.
+
+=cut
